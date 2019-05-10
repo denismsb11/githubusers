0713034a326c79e98c1f86c064e9b8d12312a75f
Denis Menezes <denismsb11@gmail.com>
2019-05-10 17:38:34 -0300

    first commit

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+    <div className="header-hunt">
+        <h1>GithubUsers</h1>
+    </div>
+  );
+}
+
+export default Header;
